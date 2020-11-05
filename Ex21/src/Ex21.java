7b2f6637496765649989793e9d104dca6e025341
Matt Hurt <mhurt@msudenver.edu>
2020-11-04 19:13:52 -0700

    Add methods

--- Ex21/src/Ex21.java
+++ Ex21/src/Ex21.java
@@ -4,6 +4,85 @@ import java.util.Scanner;
 
 class Ex21{
     final static int INF = 99999;
+
+    public static int solve(int[] A) {
+        int[][] MV = new int[A.length][A.length];
+        String[][] table = initTable(A.length);
+
+        for (int interval = 0; interval < A.length; interval++) {
+            for (int i = 0, j = interval; j < A.length; i++, j++) {
+                int a, b, c;
+                if (i + 2 <= j) {
+                    a = MV[i + 2][j];
+                } else {
+                    a = 0;
+                }
+                //////////////////////////////////
+                if (i + 1 <= j - 1) {
+                    b = MV[i + 1][j - 1];
+                } else {
+                    b = 0;
+                }
+                //////////////////////////////////
+                if (i <= j - 2) {
+                    c = MV[i][j - 2];
+                } else {
+                    c = 0;
+                }
+                //////////////////////////////////
+                MV[i][j] = Math
+                        .max(A[i] + Math.min(a, b), A[j] + Math.min(b, c));
+                int F = A[i] + Math.min(a, b);
+                int L = A[j] + Math.min(b, c);
+                if (F > L) {
+                    table[i][j] = MV[i][j] + " (F)";
+                } else if (F == L) {
+                    table[i][j] = MV[i][j] + " (*)";
+                } else {
+                    table[i][j] = MV[i][j] + " (L)";
+                }
+            }
+        }
+        printTable(table);
+        return MV[0][A.length - 1];
+    }
+    private static String[][] initTable(int size) {
+        String[][] table = new String[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                table[i][j] = "0 (-)";
+            }
+        }
+        return table;
+    }
+    private static void printTable(String[][] table) {
+        for (int i = 0; i < table.length; i++) {
+            if (i == 0) {
+                System.out.printf("%5s", "");
+                for (int k = 0; k < table.length; k++) {
+                    System.out.printf("%10d ", (k + 1));
+                }
+                System.out.println();
+            }
+            System.out.printf("%5d %s", (i + 1), "|");
+            for (int j = 0; j < table.length; j++) {
+                System.out.printf("%11s", table[i][j]);
+            }
+            System.out.println("\n");
+        }
+    }
+    private static int[] getUserInput() {
+        List<Integer> numbers = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter some integers, separated by a space or comma, press enter twice to continue.");
+        String[] next = scanner.nextLine().split("[\\s,]");
+        int[] numArray = new int[next.length];
+        for (int i = 0; i < next.length; i++) {
+            numArray[i] = Integer.parseInt(next[i]);
+        }
+        return numArray;
+    }
+
     static int optimalStrategy(int[] arr, int n)
     {
         // Create a table F to store solutions of subproblems
@@ -48,8 +127,6 @@ class Ex21{
             System.out.println("\n");
         }
     }
-
-
 
     public static void main(String[] args)
     {
